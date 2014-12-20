f3be4906190b259a08995e1b8b8e888e0d87aab6
Chris Paiano <chris.paiano@gmail.com>
2014-12-20 00:40:00 -0800

    -Updated installLinuxJoystickMapper.sh with a specific cowsay
    Former-commit-id: 42cd25a078803879e67a626ee518605aaedd2275 [formerly 628a6beca56e5fd665c0738bc7f3db9a3ddc9505]
    Former-commit-id: 494e1d0a53276050550e4bdfeb22f7334cdd9e29

--- bin/installLinuxJoystickMapper.sh
+++ bin/installLinuxJoystickMapper.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cowsay Installing Linux Joystick Mapper...
+cowsay -f dragon Installing Linux Joystick Mapper...
 echo =====================> Installing Linux Joystick Mapper
 sayWithGoogle Now installing Linux Joystick Mapper you have no chance to survive make your time
-pushd ~/primestationone
+pushd ~
 tar xfvz joymap-0.2.tar.gz
 cd joymap-0.2
 sudo make
