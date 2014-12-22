dbeba0f3a768b39dfe863143241c7aac4a82aeac
Chris Paiano <chris.paiano@gmail.com>
2014-12-21 19:21:49 -0800

    -Fixed bad removal of current vice emulator and added cleanup phase to c64 install script
    Former-commit-id: e539d5f015360e118e423914675facdce418d860
    Former-commit-id: bf3ef1e8f39d6457d2f04e5cfb4326e556396d01

--- bin/installC64emulator.sh
+++ bin/installC64emulator.sh
@@ -6,7 +6,7 @@ sudo ~/RetroPie-Setup/retropie_packages.sh vice remove
 rootdir="/opt/retropie"
 source "/home/pi/primestationone/lib/helpers.sh"
 sudo mkdir -p "$rootdir/emulators"
-sudo -rf "$rootdir/emulators/vice-2.4"
+sudo rm -rf "$rootdir/emulators/vice-2.4"
 sudo mkdir "$rootdir/emulators/vice-2.4"
 
 sudo apt-get -y install vim libice-dev libreadline-dev libsdl1.2-dev libxt-dev libxmu-dev libxaw7-dev libx11-dev libgtkglext1
@@ -38,4 +38,8 @@ sudo cp -av vice-1.5-roms/data/* /usr/lib/vice/
 setESSystem "C64" "c64" "~/RetroPie/roms/c64" ".crt .CRT .d64 .D64 .g64 .G64 .t64 .T64 .tap .TAP .x64 .X64 .zip .ZIP" "$rootdir/supplementary/runcommand/runcommand.sh 4 \"x64 -sdlbitdepth 16 %ROM%\"" "c64" "c64"
 cowsay -f stegosaurus Installing C64 ROMS for various models...
 sudo ~/RetroPie-Setup/retropie_packages.sh c64roms
+
+echo Cleaning up...
+rm -rf ~/vice*
+
 popd
