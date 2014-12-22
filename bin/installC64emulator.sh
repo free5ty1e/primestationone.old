5a8e6e30fd5701ded81f72e1c3ddf3f134a8a1d6
Chris Paiano <chris.paiano@gmail.com>
2014-12-21 19:18:07 -0800

    -Updated emulation station config generation to be correct for installed c64 emulator once script finishes
    Former-commit-id: 2b09c69ae726b2f0e4dc3f120bb6c4b5ce4abfe0
    Former-commit-id: 7ff926655413b2a7f3924131814a583511809943

--- bin/installC64emulator.sh
+++ bin/installC64emulator.sh
@@ -9,11 +9,13 @@ sudo mkdir -p "$rootdir/emulators"
 sudo -rf "$rootdir/emulators/vice-2.4"
 sudo mkdir "$rootdir/emulators/vice-2.4"
 
+sudo apt-get -y install vim libice-dev libreadline-dev libsdl1.2-dev libxt-dev libxmu-dev libxaw7-dev libx11-dev libgtkglext1
+
 wget http://www.frank-buss.de/raspberrypi/vice_2.3.dfsg-4_armhf.deb
-dpkg -i vice_2.3.dfsg-4_armhf.deb
 wget http://www.zimmers.net/anonftp/pub/cbm/crossplatform/emulators/VICE/old/vice-1.5-roms.tar.gz
 tar -xvzf vice-1.5-roms.tar.gz
-cp -a vice-1.5-roms/data/* /usr/lib/vice/
+sudo dpkg -i vice_2.3.dfsg-4_armhf.deb
+sudo cp -av vice-1.5-roms/data/* /usr/lib/vice/
 
 ##TODO: Fix so this only happens if the entry does not already exist
 #sudo su
@@ -28,16 +30,12 @@ cp -a vice-1.5-roms/data/* /usr/lib/vice/
 #cp ~/primestationone/vice-2.4.12.tar.gz ~/vice-2.4.tar.gz
 #tar xzvf ~/vice-2.4.tar.gz -C ~/
 #sudo cp -rv ~/vice-2.4.12/* "$rootdir/emulators/vice-2.4/"
-#sudo apt-get -y install vim libice-dev libreadline-dev libsdl1.2-dev libxt-dev libxmu-dev libxaw7-dev libx11-dev
 #pushd "$rootdir/emulators/vice-2.4"
 #sudo ./configure --prefix="$rootdir/emulators/vice-2.4/installdir" --enable-sdlui --without-pulse --with-sdlsound -with-x=no
 #sudo make
 #popd
 
-
-
-
-setESSystem "C64" "c64" "~/RetroPie/roms/c64" ".crt .CRT .d64 .D64 .g64 .G64 .t64 .T64 .tap .TAP .x64 .X64 .zip .ZIP" "$rootdir/supplementary/runcommand/runcommand.sh 4 \"$rootdir/emulators/vice-2.4/installdir/bin/x64 -sdlbitdepth 16 %ROM%\"" "c64" "c64"
+setESSystem "C64" "c64" "~/RetroPie/roms/c64" ".crt .CRT .d64 .D64 .g64 .G64 .t64 .T64 .tap .TAP .x64 .X64 .zip .ZIP" "$rootdir/supplementary/runcommand/runcommand.sh 4 \"x64 -sdlbitdepth 16 %ROM%\"" "c64" "c64"
 cowsay -f stegosaurus Installing C64 ROMS for various models...
 sudo ~/RetroPie-Setup/retropie_packages.sh c64roms
 popd
