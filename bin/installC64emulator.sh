53a4988513f00505cb888a0cc045c0e3cec201dd
Chris Paiano <chris.paiano@gmail.com>
2014-12-21 19:42:00 -0800

    -Reenabled installC64emulator.sh now that it works and also bonus, doesn't break an existing C64 build even if it fails -Updated C64 install script to be aware of its failures.  Now with TODOs!
    Former-commit-id: 1da7b3aa8f1a56c1982f34572ac30362a9270234
    Former-commit-id: 99381abad47f3c57d5f2501e1597ed07953d2df5

--- bin/installC64emulator.sh
+++ bin/installC64emulator.sh
@@ -7,12 +7,15 @@ source "/home/pi/primestationone/lib/helpers.sh"
 
 sudo apt-get -y install vim libice-dev libreadline-dev libsdl1.2-dev libxt-dev libxmu-dev libxaw7-dev libx11-dev libgtkglext1
 
+echo Installing VICE C64 emu v2.3...
 wget http://www.frank-buss.de/raspberrypi/vice_2.3.dfsg-4_armhf.deb
 wget http://www.zimmers.net/anonftp/pub/cbm/crossplatform/emulators/VICE/old/vice-1.5-roms.tar.gz
 tar -xvzf vice-1.5-roms.tar.gz
 sudo dpkg -i vice_2.3.dfsg-4_armhf.deb
 sudo cp -av vice-1.5-roms/data/* /usr/lib/vice/
 
+
+###TODO: Figure out how to get a newer version of the C64 emulator to build and install on the Pi!
 ##TODO: Fix so this only happens if the entry does not already exist
 #sudo su
 #echo 'deb http://ftp.uk.debian.org/debian/ squeeze main contrib' >> /etc/apt/sources.list
@@ -31,11 +34,13 @@ sudo cp -av vice-1.5-roms/data/* /usr/lib/vice/
 #sudo make
 #popd
 
+
+#TODO: Debug the below command so that it actually works from outside the retropie setup script context:
 setESSystem "C64" "c64" "~/RetroPie/roms/c64" ".crt .CRT .d64 .D64 .g64 .G64 .t64 .T64 .tap .TAP .x64 .X64 .zip .ZIP" "$rootdir/supplementary/runcommand/runcommand.sh 4 \"x64 -sdlbitdepth 16 %ROM%\"" "c64" "c64"
+
+
 cowsay -f stegosaurus Installing C64 ROMS for various models...
 sudo ~/RetroPie-Setup/retropie_packages.sh c64roms
-
 echo Cleaning up...
 rm -rf ~/vice*
-
 popd
