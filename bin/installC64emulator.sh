002eaa0804c465ec47cb583bd5e9f46a360bb66e
Chris Paiano <chris.paiano@gmail.com>
2014-12-21 19:05:15 -0800

    -Attempt at installing c64 emulator via script again...
    Former-commit-id: 18b1f831afabbabd9908d375b35bc448ac45f9f3
    Former-commit-id: 670e68507e8c552d3e9676e37b5e177867985252

--- bin/installC64emulator.sh
+++ bin/installC64emulator.sh
@@ -8,14 +8,26 @@ source "/home/pi/primestationone/lib/helpers.sh"
 sudo mkdir -p "$rootdir/emulators"
 sudo -rf "$rootdir/emulators/vice-2.4"
 sudo mkdir "$rootdir/emulators/vice-2.4"
-cp ~/primestationone/vice-2.4.12.tar.gz ~/vice-2.4.tar.gz
-tar xzvf ~/vice-2.4.tar.gz -C ~/
-sudo cp -rv ~/vice-2.4.12/* "$rootdir/emulators/vice-2.4/"
-sudo apt-get -y install vim libice-dev libreadline-dev libsdl1.2-dev libxt-dev libxmu-dev libxaw7-dev libx11-dev
-pushd "$rootdir/emulators/vice-2.4"
-sudo ./configure --prefix="$rootdir/emulators/vice-2.4/installdir" --enable-sdlui --without-pulse --with-sdlsound -with-x=no
-sudo make
-popd
+
+#TODO: Fix so this only happens if the entry does not already exist
+echo 'deb http://ftp.uk.debian.org/debian/ squeeze main contrib' >> /etc/apt/sources.list
+#sudo sed 's/^avpgw/new text/; t; s/^av/new text/; t; s/^/new text/' file
+#sudo sed '/deb http://ftp.uk.debian.org/debian/ squeeze main/c Linux Sysadmin - Scripting' /etc/apt/sources.list
+sudo apt-get update
+sudo apt-get install vice
+
+#cp ~/primestationone/vice-2.4.12.tar.gz ~/vice-2.4.tar.gz
+#tar xzvf ~/vice-2.4.tar.gz -C ~/
+#sudo cp -rv ~/vice-2.4.12/* "$rootdir/emulators/vice-2.4/"
+#sudo apt-get -y install vim libice-dev libreadline-dev libsdl1.2-dev libxt-dev libxmu-dev libxaw7-dev libx11-dev
+#pushd "$rootdir/emulators/vice-2.4"
+#sudo ./configure --prefix="$rootdir/emulators/vice-2.4/installdir" --enable-sdlui --without-pulse --with-sdlsound -with-x=no
+#sudo make
+#popd
+
+
+
+
 setESSystem "C64" "c64" "~/RetroPie/roms/c64" ".crt .CRT .d64 .D64 .g64 .G64 .t64 .T64 .tap .TAP .x64 .X64 .zip .ZIP" "$rootdir/supplementary/runcommand/runcommand.sh 4 \"$rootdir/emulators/vice-2.4/installdir/bin/x64 -sdlbitdepth 16 %ROM%\"" "c64" "c64"
 cowsay -f stegosaurus Installing C64 ROMS for various models...
 sudo ~/RetroPie-Setup/retropie_packages.sh c64roms
