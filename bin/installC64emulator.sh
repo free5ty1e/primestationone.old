4579af2f0b0643db321518432c1b42d2bef2fa01
Chris Paiano <chris.paiano@gmail.com>
2014-12-21 17:24:31 -0800

    -Created lib folder for helpful function modules to include -Created installC64emulator.sh script and used in updatePrimestationSpecificRetroPiePackages because RetroPie's c64 build script is broken
    Former-commit-id: edb5e0d73dc92e3d235c35f440fcfe4bbb8d1aa9
    Former-commit-id: 313c8ea2eff3576a00bf6988726e52c7024ee892

---
+++ bin/installC64emulator.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+rootdir="/opt/retropie"
+
+source "~/primestationone/lib/helpers.sh"
+
+mkdir -p "$rootdir/emulators"
+
+cowsay -f stegosaurus Updating C64 emulators...
+sudo ~/RetroPie-Setup/retropie_packages.sh vice remove
+pushd ~
+wget -O vice-2.4.tar.gz http://downloads.sourceforge.net/project/vice-emu/development-releases/vice-2.4.12.tar.gz?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fvice-emu%2Ffiles%2Fdevelopment-releases%2F&ts=1419209412&use_mirror=superb-dca3
+tar xzvf vice-2.4.tar.gz -C "$rootdir/emulators/"
+rm vice-2.4.tar.gz
+
+sudo apt-get -y install vim libice-dev libreadline-dev libsdl1.2-dev libxt-dev libxmu-dev libxaw7-dev libx11-dev
+
+pushd "$rootdir/emulators/vice-2.4"
+sudo ./configure --prefix="$rootdir/emulators/vice-2.4/installdir" --enable-sdlui --without-pulse --with-sdlsound -with-x=no
+sudo make
+popd
+
+setESSystem "C64" "c64" "~/RetroPie/roms/c64" ".crt .CRT .d64 .D64 .g64 .G64 .t64 .T64 .tap .TAP .x64 .X64 .zip .ZIP" "$rootdir/supplementary/runcommand/runcommand.sh 4 \"$rootdir/emulators/vice-2.4/installdir/bin/x64 -sdlbitdepth 16 %ROM%\"" "c64" "c64"
+
+cowsay -f stegosaurus Installing C64 ROMS (BIOSes) for various models...
+sudo ~/RetroPie-Setup/retropie_packages.sh c64roms
+
+popd
