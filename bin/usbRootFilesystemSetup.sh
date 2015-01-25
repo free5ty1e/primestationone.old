6625db0b5088417247139bd7cc55825056087857
Chris Paiano <chris.paiano@gmail.com>
2015-01-25 14:06:00 -0800

    -Even more accurate instructions for usbRootFilesystemSetup.sh
    Former-commit-id: 7446729f09ed9e6724b1b3101e4575b8bf2e9eb3
    Former-commit-id: ff55d7d09fe51e3f8e5447ba3d50c138f3e4ae7a

--- bin/usbRootFilesystemSetup.sh
+++ bin/usbRootFilesystemSetup.sh
@@ -11,7 +11,7 @@ sudo umount /dev/sda1
 echo Now we need to remove all partitions on the USB drive.
 echo List partitions by typing p ENTER
 echo Then delete all partitions in reverse order as necessary, full delete commands are:
-echo d 4 ENTER d 3 ENTER d 2 ENTER d ENTER
+echo d ENTER 4 ENTER d ENTER 3 ENTER d ENTER 2 ENTER d ENTER
 echo Then press n p ENTER ENTER ENTER w ENTER to write changes...
 sudo fdisk /dev/sda1
 
