49d01236a0df6ce920dac7ddf3acc58cc7141629
Chris Paiano <chris.paiano@gmail.com>
2015-01-25 13:00:26 -0800

    -Moar info in usbRootFilesystemSetup.sh
    Former-commit-id: c95d353452a0983d7995dd0d2f67ee2c671c3cba
    Former-commit-id: afcd1d417a3056f894904ac6d48ff48ed72db020

--- bin/usbRootFilesystemSetup.sh
+++ bin/usbRootFilesystemSetup.sh
@@ -2,7 +2,11 @@
 cowsay -f eyes Configuring first USB stick to be our new root filesystem!  Using /dev/sda1...
 df -h
 ls /media/usb0
-read -p "Press any key to continue or CTRL-C to cancel using /dev/sda1... " -n1 -s
 
-#sudo umount /dev/sda1
+read -p "Press any key to continue -- DO NOT RUN THIS IF YOU ALREADY ARE RUNNING YOUR PI ROOT FROM USB! -- or CTRL-C to cancel using /dev/sda1... " -n1 -s
+
+echo Unmounting USB drive...
+sudo umount /dev/sda1
+
+
 
