c2a46c05ea8d4811337892f96ec7abd719fafab2
Chris Paiano <chris.paiano@gmail.com>
2015-01-25 12:49:08 -0800

    -Fixed pause command in usbRootFilesystemSetup.sh
    Former-commit-id: f2d80193a572dc1e8b21e268fe4cc9d7a9bd15c4
    Former-commit-id: dbb34bd02cf745b3b6f922548280e555cade6b9b

--- bin/usbRootFilesystemSetup.sh
+++ bin/usbRootFilesystemSetup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-cowsay -f eyes Configuring first USB stick to be our new root filesystem!
+cowsay -f eyes Configuring first USB stick to be our new root filesystem!  Using /dev/sda1...
 df -h
 ls /media/usb0
-pause
+read -p "Press any key to continue or CTRL-C to cancel using /dev/sda1... " -n1 -s
 
+#sudo umount /dev/sda1
 
