62acc81579d5688f78fe589676047902e3115097
Chris Paiano <chris.paiano@gmail.com>
2015-01-25 13:29:20 -0800

    -Now usbRootFilesystemSetup.sh actually does the thing, and has alternate versions of the files to make root filesystem boot from USB.  Will require some user intervention to clear and create new partition on the USB drive but instructions are provided on screen...
    Former-commit-id: 4477201b2ee203c96585967d8063d74d698a7de4
    Former-commit-id: 041def743ba6ed12bb4ac70bcf709387c9a4354d

--- bin/usbRootFilesystemSetup.sh
+++ bin/usbRootFilesystemSetup.sh
@@ -8,5 +8,32 @@ read -p "Press any key to continue -- DO NOT RUN THIS IF YOU ALREADY ARE RUNNING
 echo Unmounting USB drive...
 sudo umount /dev/sda1
 
+echo Now we need to remove all partitions on the USB drive, keep pressing d then ENTER until no partitions remain... then press n p ENTER ENTER w ENTER to write changes...
+sudo fdisk /dev/sda1
 
+echo Making EXT4 filesystem on USB drive...
+sudo mkfs.ext4 /dev/sda1
 
+echo Check new filesystem...answer yes to any autofix prompts...
+sudo e2fsck /dev/sda1
+
+echo Mounting new USB filesystem...
+sudo mount /dev/sda1 /media/usb0
+
+echo Checking new storage volumes...
+df -h
+
+echo Now actually transferring entire root filesystem to USB drive so we can boot from it...using pv with estimate of 4GB to transfer, so do not be alarmed if the progress does not reach 100 percent...
+#sudo dd if=/dev/root of=/dev/sda1 bs=4M
+pv --size 4000000000 /dev/root | sudo dd bs=4M of=/dev/sda1
+
+#echo Now expanding filesystem to fill USB drive capacity...
+#sudo resize2fs -p /dev/sda1
+#df -h
+
+echo Now replacing fstab and boot cmdline with versions that mount the USB as the root filesystem instead of the 2nd partition of the SD card (which we should be able to safely remove after this is complete...)
+sudo cp -v ~/primestationone/reference/fstabForUsb /etc/fstab
+sudo cp -v ~/primestationone/reference/cmdlineForUsb.txt /boot/cmdline.txt
+
+read -p "Press any key to continue rebooting... " -n1 -s
+restart
