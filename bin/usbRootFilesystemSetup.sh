976960de59de640bbaf6390b08e8d0c72139180f
Chris Paiano <chris.paiano@gmail.com>
2015-01-25 13:35:53 -0800

    -Updated onscreen instructions for usbRootFilesystemSetup.sh to be accurate with most scenarios -Added required package pv install to see progress during root filesystem transfer
    Former-commit-id: 524ffe41fa1b6a63937ae87c0f777efb679264ce
    Former-commit-id: 4a08e287b88533124b2321c105571a171aee9244

--- bin/usbRootFilesystemSetup.sh
+++ bin/usbRootFilesystemSetup.sh
@@ -8,7 +8,10 @@ read -p "Press any key to continue -- DO NOT RUN THIS IF YOU ALREADY ARE RUNNING
 echo Unmounting USB drive...
 sudo umount /dev/sda1
 
-echo Now we need to remove all partitions on the USB drive, keep pressing d then ENTER until no partitions remain... then press n p ENTER ENTER w ENTER to write changes...
+echo Now we need to remove all partitions on the USB drive.
+echo List partitions by typing p ENTER
+echo Then press d <partition number> ENTER until no partitions remain (d 4 ENTER d 3 ENTER d 2 ENTER d ENTER).
+echo Then press n p ENTER ENTER ENTER w ENTER to write changes...
 sudo fdisk /dev/sda1
 
 echo Making EXT4 filesystem on USB drive...
@@ -23,6 +26,9 @@ sudo mount /dev/sda1 /media/usb0
 echo Checking new storage volumes...
 df -h
 
+echo Installing package progressview pv so we can see something happening during the next big operation...
+sudo apt-get install -y pv
+
 echo Now actually transferring entire root filesystem to USB drive so we can boot from it...using pv with estimate of 4GB to transfer, so do not be alarmed if the progress does not reach 100 percent...
 #sudo dd if=/dev/root of=/dev/sda1 bs=4M
 pv --size 4000000000 /dev/root | sudo dd bs=4M of=/dev/sda1
@@ -31,7 +37,7 @@ pv --size 4000000000 /dev/root | sudo dd bs=4M of=/dev/sda1
 #sudo resize2fs -p /dev/sda1
 #df -h
 
-echo Now replacing fstab and boot cmdline with versions that mount the USB as the root filesystem instead of the 2nd partition of the SD card (which we should be able to safely remove after this is complete...)
+echo Now replacing fstab and boot cmdline with versions that mount the USB as the root filesystem instead of the 2nd partition of the SD card, which we should be able to safely remove after this is complete...
 sudo cp -v ~/primestationone/reference/fstabForUsb /etc/fstab
 sudo cp -v ~/primestationone/reference/cmdlineForUsb.txt /boot/cmdline.txt
 
